3e3e69f1d1ca76bb0d2bd4b16543f59527d99388
Rob van Gils <rolorob@live.nl>
2018-02-06 19:39:01 +0100

    initial checkin project

---
+++ src/ReadingGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReadingGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
